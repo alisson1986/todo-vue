b79c0514c5e0197149f67f5766a02a19870668c6
Alisson <alissonsk8@gmail.com>
2024-08-21 12:14:59 -0300

    componentizando a aplicação

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const props = defineProps(['tarefasPendentes']);
+
+</script>
+
+<template>
+    <header class="p-5 bm-4 mt-4 bg-light rounded-3">
+      <h1>Minhas tarefas</h1>
+      <p v-if="props.tarefasPendentes > 0">
+        Você possui {{ props.tarefasPendentes }} tarefas pendentes.
+      </p>
+      <p v-else>
+        Você nao possui tarefas pendentes.
+      </p>
+      
+    </header>
+</template>
